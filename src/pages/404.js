cdcb57b510c79b01d58ea9bf05be16acd57131c3
David Davini <dndavini3.14@gmail.com>
2020-06-25 18:47:29 -0700

    added moving button!

--- src/pages/404.js
+++ src/pages/404.js
@@ -3,7 +3,27 @@ import React from "react"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
+// Python
+// f = (lambda x : x**2)
+// which is the same as
+// def f(x):
+//    return x**2
+// x = [i**2 for i in range(10)] -- not available in JS :(
+// Javascript
+// var f = ( (x) => x*x )
+
+// Python
+// a = 5
+// a = 3
+// Javascript
+// var a = 5  (or let, const)
+// a = 3
+// var -- function-scoped
+// let -- block-scoped (brackets)
+// const -- just a constant (no reassignment) thats block-scoped
+
 const NotFoundPage = () => (
+  // JSX -- just HTML with some React (HTML++)
   <Layout>
     <SEO title="404: Not found" />
     <h1>NOT FOUND</h1>
---
+++ src/pages/tic-tac-toe.js
@@ -0,0 +1,69 @@
+import React from "react"
+
+import { css } from "@emotion/core"
+
+// === -- test that these are EXACTLY equal (same type)
+// == -- test equality, allowing for type differences
+
+class MovingButton extends React.Component {
+  constructor(props) {
+    // props.children -- array of React Components nested inside of this guy
+    // props.prop1, props.prop2, etc...
+    super(props)
+    this.state = { padX: 0, padY: 0 }
+  }
+
+  handleClick() {
+    const newPadX = 1000 * Math.random()
+    const newPadY = 100 * Math.random()
+    console.log(newPadX)
+    this.setState((state, props) => ({ padX: newPadX, padY: newPadY }))
+  }
+  // setState => render
+  // more like an updateState
+
+  render() {
+    const style = `margin: ${this.state.padX}px ${this.state.padY}px`
+    return (
+      <button css={css(style)} onClick={() => this.handleClick()}>
+        ;D
+      </button>
+    )
+  }
+}
+
+function clicky(e) {
+  e.preventDefault()
+  alert("hey dont do that!")
+}
+
+// props -- immutable params given to Component on creation
+// state -- changing internal state of componente
+
+const TicTacToePage = () => (
+  // Syntactic Sugar
+  <div>
+    <h1
+      css={css`
+        width: 300px;
+        border: 15px solid green;
+        padding: 50px;
+        margin: 20px;
+        text-decoration: underline overline;
+      `}
+    >
+      Hello there lets play!
+    </h1>
+    {/*<button onClick={clicky}>Click me!</button>*/}
+    <MovingButton />
+  </div>
+)
+
+export default TicTacToePage
+
+// JS has 2 diff types of imports/exports
+// import mainFunction from "mymodule"  -- unnamed import
+// import { aFunction, anotherFunction } from "mymodule" -- named import
+
+// export default mainFunction -- unnamed export
+// exports.aFunction = aFunction -- named export
